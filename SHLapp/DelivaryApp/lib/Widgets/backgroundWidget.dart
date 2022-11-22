0e5eeed6218650b1c74d5c81cfbb047ba50f4341
fayrooz tayyah <municipaltynablus@gmail.com>
2022-11-22 08:07:31 +0200

    app

---
+++ SHLapp/DelivaryApp/lib/Widgets/backgroundWidget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../Util/Constants.dart';
+
+class background_Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+          color: CustomColors.backgroundColor,
+
+
+      ),
+
+
+
+    );
+  }
+}
